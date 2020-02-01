9d37fae468588a007cdc3222941fe563df07d602
oliver <oliver.burkhard@gmail.com>
2020-02-01 22:02:11 +0100

    Accept put requests

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+comments = []
+
+
+@app.route('/', methods=["GET", "POST"])
 def render_template1():
-    return render_template("template1.html")
+    if request.method == "GET":
+        return render_template("template1.html", comments=comments)
+
+    comments.append(request.form["contents"])
+    return redirect(url_for('render_template1'))
 
 
 if __name__ == '__main__':
