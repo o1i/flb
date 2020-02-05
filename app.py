57eca9786446efd1034d378b189f35845b57483f
oliver <oliver.burkhard@gmail.com>
2020-02-05 20:49:06 +0100

    Add DB Model

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="Althir",
+    password="lernbuero_db_pw",
+    hostname="Althir.mysql.pythonanywhere-services.com",
+    databasename="Althir$default",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
 
 
 comments = []
