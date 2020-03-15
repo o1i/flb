81acabd2ebd3c30004f0442ed8d5e5c8f4be1cb8
oliver <oliver.burkhard@gmail.com>
2020-03-15 12:55:45 +0100

    Add Frontend build

---
+++ build/precache-manifest.bc6184a834064386cc376d514a6c562e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2968d6468614e6a630d9fe9803b7cf32",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0c0392276ded83ae96dd",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6421d33bcacf36886e31",
+    "url": "./static/css/main.7a54e7b1.chunk.css"
+  },
+  {
+    "revision": "0c0392276ded83ae96dd",
+    "url": "./static/js/2.ccb071d4.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "./static/js/2.ccb071d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6421d33bcacf36886e31",
+    "url": "./static/js/main.f48d4f16.chunk.js"
+  },
+  {
+    "revision": "8fab6f763a70be500603",
+    "url": "./static/js/runtime-main.2fd2eb2e.js"
+  }
+]);
