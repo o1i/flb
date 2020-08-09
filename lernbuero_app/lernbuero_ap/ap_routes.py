18d3818eaa0aad3bd4b0271cd37a822b0bed9432
oliver <oliver.burkhard@gmail.com>
2020-08-09 18:09:38 +0200

    Add user route

--- lernbuero_app/lernbuero_ap/ap_routes.py
+++ lernbuero_app/lernbuero_ap/ap_routes.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 ap_bp = Blueprint("ap_bp", __name__, template_folder="templates", static_folder="static")
 
 
-@ap_bp.route('/api/v1/ap/gruppe/', methods=["GET", "POST"])
+@ap_bp.route('/api/v1/ap/gruppe/', methods=["GET", "POST", "DELETE"])
 @jwt_required
 def gruppe():
     user_cred = get_jwt_identity()
@@ -32,12 +32,24 @@ def gruppe():
                     db.session.add(Gruppe(name=g["name"]))
             db.session.commit()
         except Exception:
+            db.session.rollback()
             pass
+    if request.method == "DELETE":
+        try:
+            if not isinstance(request.json, list):
+                return "invalid request", 400
+            delete_groups = db.session.query(Gruppe).filter(Gruppe.id.in_(request.json)).all()
+            for g in delete_groups:
+                db.session.delete(g)
+            db.session.commit()
+        except:
+            db.session.rollback()
+                
     gruppen = Gruppe.query.all()
     return jsonify([{"name": g.name, "id": g.id} for g in gruppen]), 200
 
 
-@ap_bp.route('/api/v1/ap/block/', methods=["GET", "POST"])
+@ap_bp.route('/api/v1/ap/block/', methods=["GET", "POST", "DELETE"])
 @jwt_required
 def block():
     user_cred = get_jwt_identity()
@@ -48,7 +60,7 @@ def block():
         try:
             if not isinstance(request.json, list):
                 return "invalid request", 400
-            for b in request.json["blocks"]:
+            for b in request.json:
                 if "id" in b.keys:
                     block = Block.query.get(b["id"])
                     if "weekday" in b.keys():
@@ -64,7 +76,71 @@ def block():
                                          gruppe=gruppen[b["gruppe"]]))
             db.session.commit()
         except Exception:
+            db.session.rollback()
             pass
+
+    if request.method == "DELETE":
+        try:
+            if not isinstance(request.json, list):
+                return "invalid request", 400
+            delete_blocks = db.session.query(Block).filter(Block.id.in_(request.json)).all()
+            for b in delete_blocks:
+                db.session.delete(b)
+            db.session.commit()
+        except:
+            db.session.rollback()
+
     bloecke = Block.query.filter_by(gruppe_id=b["gruppe"])
     return jsonify([{"gruppe_id": b.gruppe_id, "weekday": b.weekday, "start": b.start,
                      "end": b.end} for b in bloecke]), 200
+
+
+@ap_bp.route('/api/v1/ap/user/', methods=["GET", "POST", "DELETE"])
+@jwt_required
+def user():
+    user_cred = get_jwt_identity()
+    if "user_type" not in user_cred.keys() or user_cred["user_type"] != "ap":
+        return "Invalid user credentials", 400
+    if request.method == "POST":
+        gruppen = dict()
+        try:
+            if not isinstance(request.json, list):
+                return "invalid request", 400
+            for u in request.json:
+                invalid = ("type" in u.keys and u["type"] == "ap")
+                if "id" in u.keys() and not invalid:
+                    user = Gruppe.query.get(u["id"])
+                    user.email = u["email"] if "email" in u.keys() else user.email
+                    user.password = u["password"] if "password" in u.keys() else user.password
+                    user.type = u["type"] if "type" in u.keys() else user.type
+                    user.gruppe_id = u["gruppe"] if "gruppe" in u.keys() else user.gruppe
+                    if "gruppe" in u.keys() and not u["gruppe"] in gruppen.keys():
+                        gruppen[u["gruppe"]] = Gruppe.query.get(u["gruppe"])
+                    user.gruppe = gruppen[u["gruppe"]]
+                elif not invalid:
+                    if "gruppe" in u.keys() and not u["gruppe"] in gruppen.keys():
+                        gruppen[u["gruppe"]] = Gruppe.query.get(u["gruppe"])
+                    db.session.add(User(email=u["email"],
+                                        password=u["password"],
+                                        type=u["type"],
+                                        gruppe_id=u["gruppe"] if "gruppe" in u.keys() else None,
+                                        gruppe=gruppen[u["gruppe"]] if "gruppe" in u.keys() else None,
+                                        ))
+            db.session.commit()
+        except Exception:
+            db.session.rollback()
+            pass
+        
+    if request.method == "DELETE":
+        try:
+            if not isinstance(request.json, list):
+                return "invalid request", 400
+            delete_users = db.session.query(User).filter(User.id.in_(request.json)).all()
+            for u in delete_users:
+                db.session.delete(u)
+            db.session.commit()
+        except:
+            db.session.rollback()
+        
+    users = User.query.all()
+    return jsonify([{"name": u.email, "id": u.id, "password": u.password, "type": u.type, "gruppe_id": u.gruppe_id} for u in users]), 200
