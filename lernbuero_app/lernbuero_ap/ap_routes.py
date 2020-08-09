f68af1d271dbe033de4c92cf927516b3d894c960
oliver <oliver.burkhard@gmail.com>
2020-08-09 13:38:45 +0200

    Add Block endpoint

--- lernbuero_app/lernbuero_ap/ap_routes.py
+++ lernbuero_app/lernbuero_ap/ap_routes.py
@@ -16,27 +16,55 @@ ap_bp = Blueprint("ap_bp", __name__, template_folder="templates", static_folder=
 
 @ap_bp.route('/api/v1/ap/gruppe/', methods=["GET", "POST"])
 @jwt_required
-def blocks():
+def gruppe():
     user_cred = get_jwt_identity()
     if "user_type" not in user_cred.keys() or user_cred["user_type"] != "ap":
         return "Invalid user credentials", 400
     if request.method == "POST":
-        if "id" in request.json.keys() and "name" in request.json.keys():
-            gruppe = Gruppe.query.get(request.json["id"])
-            gruppe.name = request.json["name"]
+        try:
+            if not isinstance(request.json, list):
+                return "invalid request", 400
+            for g in request.json:
+                if "id" in g.keys():
+                    gruppe = Gruppe.query.get(g["id"])
+                    gruppe.name = g["name"]
+                else:
+                    db.session.add(Gruppe(name=g["name"]))
             db.session.commit()
-        elif "name" in request.json.keys():
-            gruppe = Gruppe(name=request.json["name"])
-            db.session.add(gruppe)
-            db.session.commit()
+        except Exception:
+            pass
     gruppen = Gruppe.query.all()
     return jsonify([{"name": g.name, "id": g.id} for g in gruppen]), 200
 
 
 @ap_bp.route('/api/v1/ap/block/', methods=["GET", "POST"])
 @jwt_required
-def blocks():
+def block():
     user_cred = get_jwt_identity()
     if "user_type" not in user_cred.keys() or user_cred["user_type"] != "ap":
         return "Invalid user credentials", 400
-    return jsonify([]), 200
+    if request.method == "POST":
+        gruppen = dict()
+        try:
+            if not isinstance(request.json, list):
+                return "invalid request", 400
+            for b in request.json["blocks"]:
+                if "id" in b.keys:
+                    block = Block.query.get(b["id"])
+                    if "weekday" in b.keys():
+                        block.weekday = b["weekday"]
+                    if "start" in b.keys():
+                        block.start = b["start"]
+                    if "end" in b.keys():
+                        block.end = b["end"]
+                else:
+                    if b["gruppe"] not in gruppen.keys():
+                        gruppen[b["gruppe"]] = Gruppe.query.get(b["gruppe"])
+                    db.session.add(Block(weekday=b["weekday"], start=b["start"], end=b["end"], gruppe_id=b["gruppe"],
+                                         gruppe=gruppen[b["gruppe"]]))
+            db.session.commit()
+        except Exception:
+            pass
+    bloecke = Block.query.filter_by(gruppe_id=b["gruppe"])
+    return jsonify([{"gruppe_id": b.gruppe_id, "weekday": b.weekday, "start": b.start,
+                     "end": b.end} for b in bloecke]), 200
