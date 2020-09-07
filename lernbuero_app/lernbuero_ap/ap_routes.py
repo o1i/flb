3f65c0b51876bbd8833c7cd6e3a2bf4efdbf501c
oliver <oliver.burkhard@gmail.com>
2020-09-07 07:19:26 +0200

    Add Dropdown for Teachers in ap/lb

--- lernbuero_app/lernbuero_ap/ap_routes.py
+++ lernbuero_app/lernbuero_ap/ap_routes.py
@@ -230,7 +230,7 @@ def user():
         except ValueError:
             db.session.rollback()
             pass
-        
+
     if request.method == "DELETE":
         try:
             if not isinstance(request.json, list):
@@ -252,4 +252,17 @@ def user():
         "type": u.User.type,
         "gruppe": u.Gruppe.name if u[1] is not None else "",
     } for u in users]
+    return jsonify(out), 200
+
+
+@ap_bp.route('/api/v1/ap/lp/', methods=["GET"])
+@jwt_required
+def allLps():
+    user_cred = get_jwt_identity()
+    if "user_type" not in user_cred.keys() or user_cred["user_type"] != "ap":
+        return "Invalid user credentials", 400
+    lps = db.session.query(User).filter(User.type == "lp").all()
+    print(lps)
+    out = [lp.email for lp in lps]
+    print(out)
     return jsonify(out), 200
